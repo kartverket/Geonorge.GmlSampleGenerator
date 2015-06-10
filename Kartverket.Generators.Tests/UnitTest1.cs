44f6f5936d2c76dc6032d19b16370ec6b571e611
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-10 10:54:11 +0200

    Add new test project Kartverket.Generators.Tests to solution (Remove Kartverket.GmlSampleGenerator.Tests)

---
+++ Kartverket.Generators.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Kartverket.Generators.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
