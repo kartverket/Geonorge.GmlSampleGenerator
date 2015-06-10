50e1d4023f243d28debac8c29abceb1a518ca90a
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-10 16:28:02 +0200

    Add generator class for GML samples and make test comply

---
+++ Kartverket.Generators.Tests/SampleGmlGeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using NUnit.Framework;
+using Kartverket.Generators;
+using System.Xml.Linq;
+
+namespace Kartverket.Generators.Tests
+{
+    public class SampleGmlGeneratorTest
+    {
+        [Test]
+        public void ShouldGenerateSampleGmlIdenticalToGiven()
+        {
+            new SampleGmlGenerator().GenerateGml().Should().NotBeNull();
+        }
+    }
+}
