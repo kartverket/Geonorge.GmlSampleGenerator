bc45369f93dfd741eb927fe51461511167e3ea96
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-10 13:06:13 +0200

    Add test packages to Kartverket.Generators.Tests project

---
+++ Kartverket.Generators.Tests/GmlSampleGeneratorTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FluentAssertions;
+using NUnit.Framework;
+
+namespace Kartverket.Generators.Tests
+{
+    public class GmlSampleGeneratorTest
+    {
+        [Test]
+        public void ShouldGenerateGmlSampleIdenticalToGiven()
+        {
+            // Test the test-framework/-method
+            string text = "text";
+            text.Should().Be("text");
+        }
+    }
+}
