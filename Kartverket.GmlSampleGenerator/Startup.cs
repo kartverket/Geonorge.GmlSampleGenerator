db808862451cd1dd7146e6497251f5852e97444b
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-10 10:07:58 +0200

    New ASP .NET MVC 5 solution

---
+++ Kartverket.GmlSampleGenerator/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Kartverket.GmlSampleGenerator.Startup))]
+namespace Kartverket.GmlSampleGenerator
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
