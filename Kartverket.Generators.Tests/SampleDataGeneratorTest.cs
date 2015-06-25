88a7a4c3f477b156597737f160b3aafdc0e3a11f
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-25 20:36:32 +0200

    Add class SampleDataGenerator and unit tests

---
+++ Kartverket.Generators.Tests/SampleDataGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using System;
+using System.Xml.Linq;
+
+namespace Kartverket.Generators.Tests
+{
+    public class SampleDataGeneratorTest
+    {
+        [Test]
+        public void ShouldDeterminWhetherTypeIsSupported()
+        {
+            SampleDataGenerator.SupportsType("string").Should().BeTrue();
+            SampleDataGenerator.SupportsType("noType").Should().BeFalse();
+        }
+        
+        [Test]
+        public void ShouldGenerateDataAccordingToGivenTypes()
+        {
+            string stringData = (string) SampleDataGenerator.GenerateForType("string");
+            stringData.GetType().Should().Be(typeof(string));
+
+            int intData = (int) SampleDataGenerator.GenerateForType("integer");
+            intData.GetType().Should().Be(typeof(int));
+
+            double doubleData = (double) SampleDataGenerator.GenerateForType("double");
+            doubleData.GetType().Should().Be(typeof(double));
+
+            DateTime dateTimeData = (DateTime) SampleDataGenerator.GenerateForType("dateTime");
+            dateTimeData.GetType().Should().Be(typeof(DateTime));
+        }
+    }
+}
---
+++ Kartverket.Generators/SampleDataGenerator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kartverket.Generators
+{
+    public class SampleDataGenerator
+    {
+
+        public static bool SupportsType(string type)
+        {
+            return GenerateForType(type) != null;
+        }
+
+        public static object GenerateForType(string type)
+        {
+            switch (type)
+            {
+                case "string": return GenerateString();
+                case "integer": return GenerateInteger();
+                case "double": return GenerateDouble();
+                case "dateTime": return GenerateDateTime();
+                default: return null;
+            }
+        }
+
+        public static string GenerateString()
+        {
+            return "lorem ipsum";
+        }
+
+        public static int GenerateInteger()
+        {
+            return new Random().Next();
+        }
+
+        private static double GenerateDouble()
+        {
+            return new Random().NextDouble();
+        }
+
+        private static DateTime GenerateDateTime()
+        {
+            return DateTime.Now.ToLocalTime();
+        }
+    }
+}
