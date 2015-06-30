0453b5e04e62abc8adb8aba6de70341d13e3194b
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-30 10:27:41 +0200

    Add controller to handle uploads and generator results

---
+++ Kartverket.GmlSampleGenerator/Controllers/GenerateController.cs
@@ -0,0 +1,33 @@
+﻿using System.IO;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Kartverket.GmlSampleGenerator.Controllers
+{
+    public class GenerateController : Controller
+    {
+        // GET: Generate
+        public ActionResult Index()
+        {
+            return RedirectToAction("GmlFromXsd");
+        }
+
+        [HttpGet]
+        public ActionResult GmlFromXsd()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult GmlFromXsd(HttpPostedFileBase xsdfile)
+        {
+            if (xsdfile != null && xsdfile.ContentLength > 0)
+            {
+                var xsdFilename = Path.GetFileName(xsdfile.FileName);
+                // Process stream
+            }
+
+            return RedirectToAction("GmlFromXsd");
+        }
+    }
+}
