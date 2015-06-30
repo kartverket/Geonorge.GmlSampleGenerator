000465c2ea6e9ee6080fee24d37a15db7c7212e3
Jørgen Tellnes <jorgen@arkitektum.no>
2015-06-30 11:06:32 +0200

    Add view for XSD file upload

---
+++ Kartverket.GmlSampleGenerator/Scripts/file-inputelement-handling.js
@@ -0,0 +1,24 @@
+﻿// Handling of file upload elements
+
+$(document).on('change', '.btn-file :file', function () {
+    // alert("Function triggered!")
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready(function () {
+    $('.btn-file :file').on('fileselect', function (event, numFiles, label) {
+
+        var input = $(this).parents('.input-group').find(':text'),
+            log = numFiles > 1 ? numFiles + ' files selected' : label;
+
+        if (input.length) {
+            input.val(log);
+        } else {
+            if (log) alert(log);
+        }
+
+    });
+});
