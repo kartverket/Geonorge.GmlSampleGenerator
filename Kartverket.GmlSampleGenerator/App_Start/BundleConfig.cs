9acd2e062c5a62f7669c8e3fd9a8703a9b2ea141
Dag Olav <dagolav@arkitektum.no>
2015-08-21 10:38:36 +0200

    Added Geonorge layout

--- Kartverket.GmlSampleGenerator/App_Start/BundleConfig.cs
+++ Kartverket.GmlSampleGenerator/App_Start/BundleConfig.cs
@@ -24,8 +24,12 @@ namespace Kartverket.GmlSampleGenerator
                       "~/Scripts/respond.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+                     "~/Content/bootstrap.css",
+                     "~/Content/common.css",
+                     "~/Content/navbar.css",
+                     "~/Content/searchbar.css",
+                     "~/Content/custom.css",
+                     "~/Content/site.css"));
 
             // Set EnableOptimizations to false for debugging. For more information,
             // visit http://go.microsoft.com/fwlink/?LinkId=301862
---
+++ Kartverket.GmlSampleGenerator/Helpers/HtmlHelperExtensions.cs
@@ -0,0 +1,25 @@
+﻿using Kartverket.GmlSampleGenerator.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Mvc;
+
+namespace Kartverket.GmlSampleGenerator.Helpers
+{
+    public static class HtmlHelperExtensions
+    {
+        public static string ApplicationVersionNumber(this HtmlHelper helper)
+        {
+            string versionNumber = WebConfigurationManager.AppSettings["BuildVersionNumber"];
+            return versionNumber;
+        }
+
+        public static string GeonorgeUrl(this HtmlHelper helper)
+        {
+            return WebConfigurationManager.AppSettings["GeonorgeUrl"];
+        }
+    }
+}
