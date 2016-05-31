c17c0895250fa124dc0d2d0c6a1a3451972ae075
Benjamin Dehli <benjamin@arkitektum.no>
2016-05-31 13:06:34 +0200

    Add Bower and Bower components

--- Kartverket.GmlSampleGenerator/App_Start/BundleConfig.cs
+++ Kartverket.GmlSampleGenerator/App_Start/BundleConfig.cs
@@ -8,9 +8,15 @@ namespace Kartverket.GmlSampleGenerator
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-           
+            bundles.Add(new StyleBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/css/styles").Include(
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/vendor.min.css",
+                "~/Content/bower_components/kartverket-felleskomponenter/assets/css/main.min.css"
+            ));
 
-            
+            bundles.Add(new ScriptBundle("~/Content/bower_components/kartverket-felleskomponenter/assets/scripts").Include(
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/vendor.min.js",
+               "~/Content/bower_components/kartverket-felleskomponenter/assets/js/main.js"
+           ));
         }
     }
 }
